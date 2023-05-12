49dfb0aeac1a9cf0de898dc029d5717571a39d6e
Staravi <dadninele@gmail.com>
2023-05-12 08:51:42 +0800

    update

---
+++ 1Case/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArticulationPointsFinder;
+using System;
+
+namespace MainSpace
+{
+    internal class MainClass
+    {
+        private static void Main()
+        {
+            List<int> input = new List<int>(){11, 12, 15, 17, 20, 22, 25, 27, 31, 32, 33,
+                                               1, 0, 2, 3, 1, 4, 1, 4, 5, 2, 3, 3, 6, 7, 5, 
+                                               7, 5, 6, 8, 9, 7, 7};
+            List<int> dfn = new List<int> (){5, 4, 3, 1, 2, 6, 7, 8, 9, 10};
+            APF A = new APF(input);
+            A.Finding(dfn);
+        }
+    }
+}
---
+++ ArticulationPointsFinder/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ArticulationPointsFinder;
+public class APF
+{
+    private GraphReader Input { get; set; }
+    private List<int>? DFN { get; set; }
+    private List<int>? LOW { get; set; }
+    public APF(List<int> input)
+    {
+        Input = new GraphReader(input);
+    }
+
+    ///<summary>return the Articulation Points</summary>///
+    public List<int> Finding(List<int> dfn)
+    {
+        if(dfn is null) throw new NullReferenceException("DFN");
+
+        Input.SupportGraph(dfn);
+        
+        return null;
+    }
+
+    private void CalcuLow()
+    {
+
+    }
+
+}
